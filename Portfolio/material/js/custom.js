2c73b3539fe5ff1590f57cec1cb8193c6ca28fcc
Oriol Paricio <oparicio.crn@artsgrafiques.org>
2019-03-20 23:07:58 +0100

    Update custom.js

--- Portfolio/material/js/custom.js
+++ Portfolio/material/js/custom.js
@@ -141,7 +141,7 @@ $(document).ready(function () {
             },
             submitHandler: function (form) {
                 $(form).ajaxSubmit({
-                    type: "POST",
+                    type: "GET",
                     data: $(form).serialize(),
                     url: "../../process.php",
                     success: function () {
